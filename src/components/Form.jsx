5d68215821a94c235032628b8b7a96d6378e6c36
pranayadna <pranayadnaap96@gmail.com>
2024-07-16 16:55:22 +0700

    Create TemplateCV.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,80 @@
+import { useState } from "react";
+import { TemplateCV } from "./TemplateCV";
+
 function Form() {
+    const [status, setStatus] = useState("typing");
+    const [name, setName] = useState("");
+    // const [email, setEmail] = useState("");
+    // const [phone, setPhone] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setStatus("sent");
+    }
+
+    const handleNameChange = (e) => {
+        setName(e.target.value);
+    }
+
+    // const handleEmailChange = (e) => {
+    //     setEmail(e.target.value);
+    // }
+    
+    // const handlePhoneChange = (e) => {
+    //     setPhone(e.target.value);
+    // }
+
+    const isSent = status === 'sent';
+
+    if (isSent) {
+        return (
+            <div>
+                <TemplateCV 
+                    name={name}
+                    // email={email}
+                    // phone={phone}
+                />
+                {/* <p>{name}</p> */}
+            </div>
+        )
+    }
+
     return (
-        <>
-            <form action="" method="post">
-                Name: 
-                <input type="text" />
-                Email: 
-                <input type="email" name="email" id="email" />
-                Phone Number: 
-                <input type="text" />
+        <div>
+            <h1>Fill in the form to generate your CV</h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Name:{" "}
+                    <input 
+                        type="text" 
+                        value={name}
+                        onChange={handleNameChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    Email:{" "}
+                    <input 
+                        type="email" 
+                        name="email" 
+                        id="email"
+                        // value={handleEmailChange} 
+                    />
+                </label>
+                <br />
+                <label>
+                    Phone Number:{" "}
+                    <input 
+                        type="tel" 
+                        name="phone" 
+                        id="phone" 
+                        // value={handlePhoneChange}
+                    />
+                </label>
+                <br />
+                <button type="submit">Generate</button>
             </form>
-        </>
+        </div>
     )
 }
 
---
+++ src/components/TemplateCV.jsx
@@ -0,0 +1,11 @@
+function TemplateCV({ name }) {
+    return (
+        <>
+            <p>Name: {name}</p>
+            {/* <p>Email: {email}</p>
+            <p>Phone: {phone}</p> */}
+        </>
+    )
+}
+
+export { TemplateCV };
