521405029e2e027a2f8dcd444d8c7f6416e768a6
pranayadna <pranayadnaap96@gmail.com>
2024-07-16 12:57:51 +0700

    Create Form.jsx

---
+++ src/components/Form.jsx
@@ -0,0 +1,16 @@
+function Form() {
+    return (
+        <>
+            <form action="" method="post">
+                Name: 
+                <input type="text" />
+                Email: 
+                <input type="email" name="email" id="email" />
+                Phone Number: 
+                <input type="text" />
+            </form>
+        </>
+    )
+}
+
+export { Form };
